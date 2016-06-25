caa0b78c05b362cbe91221dd25beb85237e74300
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-06-24 23:04:46 -0700

    add moment example and file

---
+++ moment-examples.js
@@ -0,0 +1,20 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+now.subtract(1, 'year');
+console.log(now.format());
+console.log(now.format('h:mma'));
+console.log(now.format('MMM Do YYYY, h:mm a'));
+
+console.log(now.format('X')); //secs
+console.log(now.format('x')); //milliseconds
+// above two gives string output
+// below gives neumeric output
+console.log(now.valueOf());
+
+var timestamp = 1444247486704;
+var timestampMoment = moment.utc(timestamp); 
+console.log(timestampMoment.format()); // output will be timestamp in normal time
+console.log(timestampMoment.format('h:mm a'));
+console.log(timestampMoment.local().format('h:mm a'));
