8b060061b5f5b78ae684468b15b2ebee96a98a98
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-07-04 18:51:09 -0700

    add support for chat rooms

--- moment-examples.js
+++ moment-examples.js
@@ -9,12 +9,18 @@ console.log(now.format('MMM Do YYYY, h:mm a'));
 
 console.log(now.format('X')); //secs
 console.log(now.format('x')); //milliseconds
+console.log(typeof now.format('X')); //secs
+console.log(typeof now.format('x'));
 // above two gives string output
 // below gives neumeric output
 console.log(now.valueOf());
+console.log(typeof now.valueOf());
 
 var timestamp = 1444247486704;
 var timestampMoment = moment.utc(timestamp); 
 console.log(timestampMoment.format()); // output will be timestamp in normal time
 console.log(timestampMoment.format('h:mm a'));
 console.log(timestampMoment.local().format('h:mm a'));
+
+now.add(1, 'year');
+console.log(now.format('MMM Do YYYY, h:mm a'));
