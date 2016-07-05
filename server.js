7a521c9f3a0a4bd5533b93bb23a73f73c49ebdb9
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-07-04 19:15:29 -0700

    add @currentUsers command

--- server.js
+++ server.js
@@ -9,6 +9,28 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 /*io.on('connection', function () {
 	console.log('User connection via socket.io!');
 });*/
@@ -41,9 +63,15 @@ io.on('connection', function (socket) {
 
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
-		//socket.broadcast.emit('message', message);
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);	
+		}
+		else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+			//socket.broadcast.emit('message', message);
+		}
 	});
 
 	socket.emit('message', {
